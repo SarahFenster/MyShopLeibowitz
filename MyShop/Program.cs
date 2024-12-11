c512e32ff62734a0ee55dbec816ab7d391d8e47f
Yehudit-Leibowitz <38214938334@mby.co.il>
2024-12-11 19:18:39 +0200

    middle of homework

--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
---
+++ MyShop/_Mapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DTO;
+using Entity;
+
+namespace MyShop
+ 
+{
+    public class _Mapper : Profile
+    {
+        protected _Mapper()
+        {
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
